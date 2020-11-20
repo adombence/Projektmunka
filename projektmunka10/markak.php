c0bb4bbc587350e796c8bf3e90bf4437884aba5d
adomb <adom.bence97@gmail.com>
2020-11-20 22:08:40 +0100

    Create markak.php

---
+++ projektmunka10/markak.php
@@ -0,0 +1,22 @@
+<?php
+require('php/db.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Márkák</title>
+    <script>
+        var brand = document.location.search.replace(/^.*?\=/, '');
+        console.log(brand);
+    </script>
+</head>
+
+<body>
+
+</body>
+
+</html>
