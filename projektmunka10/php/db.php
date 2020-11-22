ca59b296690b90764ee166c27f457a0be0146a12
adomb <adom.bence97@gmail.com>
2020-11-22 22:10:21 +0100

    Update db.php

--- projektmunka10/php/db.php
+++ projektmunka10/php/db.php
@@ -9,5 +9,7 @@ $conn = mysqli_connect($servername, $username, $password, 'projektmunka6');
 // kapcsolat ellenőrzése
 if (!$conn) {
   die("kapcsolat meghíusult: " . mysqli_connect_error());
+} else {
+  echo "OK";
 }
 ?> 
