3e72f2565ebfcaa1f1bfb850025db2856fdbbe03
adomb <adom.bence97@gmail.com>
2020-11-23 22:52:50 +0100

    Update markak.php

--- projektmunka10/markak.php
+++ projektmunka10/markak.php
@@ -1,3 +1,13 @@
+<?php
+require('php/db.php');
+$marka = $_GET['marka'];
+
+$markaAdatok = "SELECT * 
+FROM `gyarto` 
+WHERE gyartoNev = '$marka';";
+$result = $conn->query($markaAdatok) or die($conn->error);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,25 +30,115 @@
     <div class="row">
         <div class="side">
             <h1>Gyártó adatok</h1>
+            <?php
+            $rows = $result->fetch_assoc();
+            $gyartoId = $rows['gyartoId'];
+            ?>
+            <table class="table1">
+                <tr>
+                    <th>gyártó:</th>
+                    <td><?php echo $rows['gyartoNev'] ?></td>
+                </tr>
+                <tr>
+                    <th>alapító(k):</th>
+                    <td><?php echo $rows['alapito'] ?></td>
+                </tr>
+                <tr>
+                    <th>alapítás éve:</th>
+                    <td><?php echo $rows['alapitasEve'] ?></td>
+                </tr>
+            </table>
+
+
         </div>
         <div class="main">
             <h1>Székhelye</h1>
+            <?php
+            mysqli_free_result($result);
+            $szekhelyAdatai  = "SELECT * FROM `szekhely` WHERE gyartoId = $gyartoId;";
+            $result = $conn->query($szekhelyAdatai) or die($conn->error);
+            $rows = $result->fetch_assoc();
+            ?>
+            <table class="table2">
+                <tr>
+                    <th>ország:</th>
+                    <td><?php echo $rows['orszag'] ?></td>
+                </tr>
+                <tr>
+                    <th>város:</th>
+                    <td><?php echo $rows['varos'] ?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="side2">
+            <h1>Legkevesebbet eladott autó</h1>
+            <?php
+            mysqli_free_result($result);
+            $legkevesebbetEladott = "SELECT * FROM `modell` WHERE gyartoId = $gyartoId ORDER BY eladottDarabszam LIMIT 1;";
+            $result = $conn->query($legkevesebbetEladott) or die($conn->error);
+            $rows = $result->fetch_assoc();
+            ?>
+            <h2><?php echo $rows['modellNev'] ?>:</h2>
+            <h2><?php echo $rows['eladottDarabszam'] ?> darab</h2>
+        </div>
+        <div class="main2">
+            <h1>Legtöbbet eladott autó</h1>
+            <?php
+            mysqli_free_result($result);
+            $legtobbetEladott = "SELECT * FROM `modell` WHERE gyartoId = $gyartoId ORDER BY eladottDarabszam DESC LIMIT 1;";
+            $result = $conn->query($legtobbetEladott) or die($conn->error);
+            $rows = $result->fetch_assoc();
+            ?>
+            <h2><?php echo $rows['modellNev'] ?>:</h2>
+            <h2><?php echo $rows['eladottDarabszam'] ?> darab</h2>
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="full2">
+            <h1>Összes eladott autó</h1>
+            <?php
+            $result = mysqli_query($conn, 'SELECT SUM(eladottDarabszam) FROM modell WHERE gyartoId =' . $gyartoId);
+            $row = mysqli_fetch_assoc($result);
+            $sum = $row['SUM(eladottDarabszam)'];
+            ?>
+            <h1><?php echo $sum ?> darab</h1>
         </div>
     </div>
     <div class="row">
         <div class="full">
             <h1>Autók</h1>
+            <table class="table">
+                <tr>
+                    <th>Modell név </th>
+                    <th>Gyártási év </th>
+                    <th>Eladott darabbszám </th>
+                </tr>
+                <?php
+                mysqli_free_result($result);
+                $modellek = "SELECT * FROM `modell` WHERE gyartoId = $gyartoId ORDER BY gyartasiEv";
+                $result = $conn->query($modellek) or die($conn->error);
+                while ($rows = $result->fetch_assoc()) {
+                ?>
+                    <tr>
+                        <td><?php echo $rows['modellNev'] ?></td>
+                        <td><?php echo $rows['gyartasiEv'] ?></td>
+                        <td><?php echo $rows['eladottDarabszam'] ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </table>
         </div>
     </div>
     <div class="footer">
         <h2>ProjekMunka10</h2>
-        <h4>készítette:</h4>
-        <h5>Ádom Bence</h5>
-        <h5>Balogh Gábor</h5>
-        <h5>Maknyik Márk</h5>
     </div>
     <script>
         document.getElementById('gyarto').innerHTML = gyarto;
+        console.log("<?php echo $gyartoId ?>");
     </script>
 </body>
 
