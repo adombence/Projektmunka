b2be190af0c67c03ccee63186f2e0ed93102d5e8
adomb <adom.bence97@gmail.com>
2020-11-23 22:52:57 +0100

    Update db.php

--- projektmunka10/php/db.php
+++ projektmunka10/php/db.php
@@ -4,12 +4,12 @@ $username = "projektmunka9";
 $password = "VAcSZf8QT3sqz7A";
 
 // kapcsolat létrehozása
-$conn = mysqli_connect($servername, $username, $password, 'projektmunka6');
+$conn = mysqli_connect($servername, $username, $password, 'projektmunka9');
 
 // kapcsolat ellenőrzése
 if (!$conn) {
   die("kapcsolat meghíusult: " . mysqli_connect_error());
 } else {
-  echo "OK";
+  /*echo "OK";*/
 }
 ?> 
