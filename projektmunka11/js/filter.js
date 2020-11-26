1d2ff58be0208f695ded5cc61992208b54bfe5db
adomb <adom.bence97@gmail.com>
2020-11-26 22:12:20 +0100

    Create filter.js

---
+++ projektmunka11/js/filter.js
@@ -0,0 +1,18 @@
+var filter = document.getElementsByClassName("filter");
+var i;
+
+for (i = 0; i < filter.length; i++) {
+    filter[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
