869149dfae1baa716f60bfca0df4f0ecf5aedaa1
adomb <adom.bence97@gmail.com>
2020-11-26 22:12:18 +0100

    Create kereses.php

---
+++ projektmunka11/kereses.php
@@ -0,0 +1,24 @@
+<?php
+require('php/db.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <button class="filter">szűrés</button>
+    <div class="panel">
+        <p>asd</p>
+    </div>
+
+    <script src="js/filter.js"></script>
+</body>
+
+</html>
