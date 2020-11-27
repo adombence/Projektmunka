600154297df77b0f6832ff91d08d24b77d868853
adomb <adom.bence97@gmail.com>
2020-11-27 15:55:52 +0100

    Update kereses.php

--- projektmunka11/kereses.php
+++ projektmunka11/kereses.php
@@ -10,15 +10,34 @@ require('php/db.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/teszt.css">
 </head>
 
 <body>
     <button class="filter">szűrés</button>
     <div class="panel">
-        <p>asd</p>
+        <form action="POST">
+            <div class="full5">
+                <h1>Keletkezés éve</h1>
+                <div class="range-box">
+                    <div id='fullbar' class="range">
+                        <div id='filteredbar' class='range__between'></div>
+                        <button id='min' class="range__button_1"></button>
+                        <button id='max' class="range__button_2"></button>
+                        <input id='inputmin' class='range_inpt1' type='number' min='1800' max='1999' value="1800">
+                        - <input id='inputmax' class='range_inpt2' type='number' min='1800' max='1999' value="1999">
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </form>
     </div>
 
     <script src="js/filter.js"></script>
+    <script src="js/range.js"></script>
 </body>
 
 </html>
