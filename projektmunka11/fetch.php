0f6a6c83ce6629d275d62579ac9123d56a42dd90
Bence Ádom <44981129+adombence@users.noreply.github.com>
2020-12-02 17:18:44 +0100

    Update fetch.php

--- projektmunka11/fetch.php
+++ projektmunka11/fetch.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "mysql.omega:3306";
-$username = "projektmunka10";
-$password = "nnmbbUv7Qibi3DS_";
+$servername = "";
+$username = "";
+$password = "";
 
 // kapcsolat létrehozása
 $conn = mysqli_connect($servername, $username, $password, 'projektmunka10');
